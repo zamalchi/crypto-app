cee7fb0541dc7df06e2dc6b4f2c2b3f6ec65adf8
Eric <zamalchi@techsquare.com>
2016-09-07 14:53:48 -0400

    sets host address flag back to -a

--- run.py
+++ run.py
@@ -8,12 +8,12 @@ from src.main import *
 
 # argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-h', help = "Host address", action="store", dest="h", required = True)
+parser.add_argument('-a', help = "Host address", action="store", dest="a", required = True)
 parser.add_argument('-p', help = "Port number", action="store", dest="p", required = True)
 
 args = parser.parse_args()
 
 port = args.p
-host = args.h
+host = args.a
 
 run(host=host, port=port, debug=True)
