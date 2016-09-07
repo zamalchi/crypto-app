f4284902b11fc4e6db8a899feaf8939d3b568830
Eric <zamalchi@techsquare.com>
2016-09-07 14:22:20 -0400

    fixes run.py by removing old code

--- run.py
+++ run.py
@@ -10,23 +10,8 @@ from src.main import *
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', help = "Port number", action="store", dest="p", required = True)
 
-parser.add_argument('-s', help = "Sender address", action="store", dest="s", required = False)
-parser.add_argument('-m', help = "Receiver address", action="store", dest="m", required = False)
-
-# parser.add_argument('-d', help = "Dev mode", action="store", dest="d", required = False) # True|1 / False|0
-
 args = parser.parse_args()
 
 port = args.p
-mailFrom = args.s
-mailTo = args.m
-# dev = args.d
-
-smtpInit(mailTo, mailFrom)
-
-# if dev == "True":
-    # reloader: restart on module file change
-    # run(reloader=True, host='localhost', port=port, debug=True)
-# else:
 
 run(host='172.16.1.254', port=port, debug=True)
